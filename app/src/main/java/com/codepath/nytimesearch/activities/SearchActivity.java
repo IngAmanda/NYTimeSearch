2efeb893dd15e8d378171ba3954e680650545808
Your Name <you@example.com>
2018-07-15 18:13:19 -0400

    gridView (image, text)

--- app/src/main/java/com/codepath/nytimesearch/activities/SearchActivity.java
+++ app/src/main/java/com/codepath/nytimesearch/activities/SearchActivity.java
@@ -1,5 +1,6 @@
-package com.codepath.nytimesearch;
+package com.codepath.nytimesearch.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,11 +11,15 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import com.codepath.nytimesearch.Article;
+import com.codepath.nytimesearch.ArticleArrayAdapter;
+import com.codepath.nytimesearch.R;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
@@ -52,6 +57,21 @@ public class SearchActivity extends AppCompatActivity {
         articles = new ArrayList<>();
         adapter = new ArticleArrayAdapter(this, articles);
         gvResults.setAdapter(adapter);
+
+        // hook up listener for grid click
+        gvResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // create an intent to display the article
+                Intent i = new Intent(getApplicationContext(), ArticleActivity.class);
+                // get the article to display
+                Article article = articles.get(position);
+                // pass in the article into intent
+                i.putExtra("article", article);
+                //launch the activity
+                startActivity(i);
+            }
+        });
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -84,6 +104,7 @@ public class SearchActivity extends AppCompatActivity {
         RequestParams params = new RequestParams();
         params.put("api-key", "09f5bd0157cf4fe1987f149bef49cd16");
         params.put("page", 0);
+        params.put("q", query);
 
         client.get(url, params, new JsonHttpResponseHandler() {
             @Override
